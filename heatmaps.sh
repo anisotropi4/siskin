94387b60da6bd10b7dd59b1d9c91586bc5a9d913
Will Deakin <will.deakin@crinstitute.org.uk>
2022-09-02 18:33:23 +0100

    update as python concatenates data

--- heatmaps.sh
+++ heatmaps.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/sh -x
 
-if [ -f heatmap.gpkg ]; then
-    rm heatmap.gpkg
-fi
-
-ogrinfo output/heatmap.gpkg -sql "VACUUM"
-
-for i in $(seq -w 64)
-do
-    ogrinfo output/heatmap-${i}.gpkg -sql "VACUUM"
-    ogr2ogr -f "gpkg" -append heatmap.gpkg output/heatmap-${i}.gpkg
-done
-
-ogr2ogr -f "gpkg" -append heatmap.gpkg output/heatmap.gpkg
 ogrinfo heatmap.gpkg -sql "VACUUM"
